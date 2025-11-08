3d2b7d3fcf66135ac2b9cbb9d9d9cdfccec6b24f
Matvey Gontar <mgontar@efl.edu>
2025-11-07 22:51:42 -0500

    fuck main

--- main.cpp
+++ main.cpp
@@ -1,31 +1,31 @@
 
 
-#include "ABDQ.hpp"
-#include "ABQ.hpp"
-#include "ABS.hpp"
-#include "Interfaces.hpp"
-#include "LinkedList.hpp"
-#include "LLDQ.hpp"
-#include "LLQ.hpp"
-#include "LLS.hpp"
-#include <iostream>
-#include <utility>
-/*
-    - LEAVE THE IFDEF GUARDS TO KEEP YOUR MAIN WITHOUT CONFLICTING WITH GRADESCOPE!
-    - Test your code in this main and run locally with the RunMain configuration in CLion, or
-    through your own command line compilation. Make sure to add flag "-DMAIN" to your compilation so this
-    main function can compile.
+// #include "ABDQ.hpp"
+// #include "ABQ.hpp"
+// #include "ABS.hpp"
+// #include "Interfaces.hpp"
+// #include "LinkedList.hpp"
+// #include "LLDQ.hpp"
+// #include "LLQ.hpp"
+// #include "LLS.hpp"
+// #include <iostream>
+// #include <utility>
+// /*
+//     - LEAVE THE IFDEF GUARDS TO KEEP YOUR MAIN WITHOUT CONFLICTING WITH GRADESCOPE!
+//     - Test your code in this main and run locally with the RunMain configuration in CLion, or
+//     through your own command line compilation. Make sure to add flag "-DMAIN" to your compilation so this
+//     main function can compile.
 
-*/
+// */
 
-int main() {
-    LLS<int> a;
-    LLQ<int> b;
-    LLDQ<int> c;
-    ABS<int> d;
-    ABQ<int> e;
-    ABDQ<int> f;
-    return 0;
-}
+// int main() {
+//     LLS<int> a;
+//     LLQ<int> b;
+//     LLDQ<int> c;
+//     ABS<int> d;
+//     ABQ<int> e;
+//     ABDQ<int> f;
+//     return 0;
+// }
 
 
